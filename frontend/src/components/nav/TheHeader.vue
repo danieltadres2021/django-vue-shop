a9f2d3584e54029fbeb239be12e8657e0db3a26f
Dani <daniaxasabraxas@gmail.com>
2020-11-19 21:37:14 -0300

    Adding ProductList route

--- frontend/src/components/nav/TheHeader.vue
+++ frontend/src/components/nav/TheHeader.vue
@@ -1,27 +1,34 @@
 <template>
-
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container d-flex align-items-center">
-      <a href="/" class="navbar-brand pt-4 pb-4 font-xl-size">Vinylsmind</a>
+      <router-link to="/">
+        <a class="navbar-brand pt-4 pb-4 font-xl-size">Vinylsmind</a>
+      </router-link>
       <button class="navbar-toggler" data-toggle="collapse" data-target="#nav">
         <span class="navbar-toggler-icon"> </span>
       </button>
       <div class="collapse navbar-collapse" id="nav">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a href="/" class="nav-link"> Catalogo </a>
+            <router-link to="/products">
+              <a class="nav-link"> Catalogo </a>
+            </router-link>
            </li>
         </ul>
       </div>
     </div>
   </nav>
-
 </template>
 
 <style scoped>
 
 .font-xl-size {
   font-size: 2rem;
+}
+
+a {
+
+  text-decoration: none;
 }
 
 </style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './pages/Home.vue'
+import Home from './pages/Home.vue';
+import ProductsList from './pages/ProductsList.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Home }
+    { path: '/', component: Home },
+    { path: '/products', component: ProductsList }
   ]
 })
 
