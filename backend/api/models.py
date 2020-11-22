996d0b266fdf3c76f5fb7c65df5893dd144044ec
Dani <daniaxasabraxas@gmail.com>
2020-11-21 21:34:09 -0300

    Creating a model to work with

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    categoryId  = models.IntegerField(primary_key=True)
+    title       = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['categoryId']
