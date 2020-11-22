8938f1708d9addd47924ef4eaf1ac95a8fe99d3e
Dani <daniaxasabraxas@gmail.com>
2020-11-21 23:05:18 -0300

    Added django views using CategorySerializer and added it to urls

---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('categories/', views.category_list),
+    path('categories/<int:pk>/', views.category_detail),
+]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from api.models import Category
+from api.serializers import CategorySerializer
+
+@csrf_exempt
+def category_list(request):
+    """
+    List all code categories, or create a new category.
+    """
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CategorySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def category_detail(request, pk):
+    """
+    Retrieve, update or delete a code category.
+    """
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CategorySerializer(category, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        category.delete()
+        return HttpResponse(status=204)
