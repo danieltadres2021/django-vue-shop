7c228a5303bc6d0d460147327538c4beb1b2d79f
Dani <daniaxasabraxas@gmail.com>
2020-11-22 22:34:15 -0300

    Working on Authentication and Permissions

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from api.models import Category
 
@@ -5,3 +6,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['categoryId', 'title']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,16 +5,37 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from api.models import Category
-from api.serializers import CategorySerializer
-from rest_framework import generics
+from django.contrib.auth.models import User
 
+from api.models import Category
+
+from api.serializers import (
+    CategorySerializer,
+    UserSerializer
+)
+
+from api.permissions import IsOwnerOrReadOnly
+
+from rest_framework import (
+    generics,
+    permissions
+)
 
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
