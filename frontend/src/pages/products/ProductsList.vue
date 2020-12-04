097f7283c0f2a201de7c6ac2ec60782f48b69da4
Dani <daniaxasabraxas@gmail.com>
2020-12-04 19:03:53 -0300

    Refactoring layout

--- frontend/src/pages/products/ProductsList.vue
+++ frontend/src/pages/products/ProductsList.vue
@@ -1,14 +1,11 @@
 <template>
 
   <base-card>
-    <h1>ProductList.vue</h1>
-
     <ul>
       <li v-for="(prod) in fetchAllProducts" v-bind:key="prod.productId">
         {{ prod }}
       </li>
     </ul>
-
   </base-card>
 
 </template>
