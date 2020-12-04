c3a41bbac94c9f5dd92a7272109b847bf3abc712
Dani <daniaxasabraxas@gmail.com>
2020-12-04 20:25:44 -0300

    Adding ProductItem styles

--- frontend/src/components/products/ProductItem.vue
+++ frontend/src/components/products/ProductItem.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <h1>ProductItem.vue</h1>
+  
+
+  <div class="card" style="width:400px">
+    <div class="card-body">
+      <h4 class="card-title">Jane Doe</h4>
+      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+    <img class="card-img-bottom" src="img_avatar6.png" alt="Card image" style="width:100%">
   </div>
+
 </template>
+
+<style scoped>
+
+</style>
