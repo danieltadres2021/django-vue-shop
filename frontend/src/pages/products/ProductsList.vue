b695a7b63590fcaa5a700dc21dab768546d4ea0a
Dani <daniaxasabraxas@gmail.com>
2020-12-04 15:34:38 -0300

    Testing the conexion with Vuex

--- frontend/src/pages/products/ProductsList.vue
+++ frontend/src/pages/products/ProductsList.vue
@@ -1,5 +1,26 @@
 <template>
+
   <base-card>
     <h1>ProductList.vue</h1>
+    {{ fetchAllProducts }}
   </base-card>
+
 </template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch('loadProducts');
+  },
+  data() {
+    return {
+      products: this.$store.getters.getProducts
+    }
+  },
+  computed: {
+    fetchAllProducts() {
+      return this.products;
+    }
+  }
+}
+</script>
