374f419d6079eb41b65489b77fb7bb0a5e265da4
Dani <daniaxasabraxas@gmail.com>
2020-12-05 19:51:19 -0300

    Added ProductItem styles

--- frontend/src/components/products/ProductItem.vue
+++ frontend/src/components/products/ProductItem.vue
@@ -1,17 +1,12 @@
 <template>
-  
-
-  <div class="card" style="width:400px">
-    <div class="card-body">
-      <h4 class="card-title">Jane Doe</h4>
-      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary">See Profile</a>
+  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mt-4">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Jane Doe</h4>
+        <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+      <img class="card-img-bottom" src="img_avatar6.png" alt="Card image" style="width:100%">
     </div>
-    <img class="card-img-bottom" src="img_avatar6.png" alt="Card image" style="width:100%">
   </div>
-
 </template>
-
-<style scoped>
-
-</style>
