543f3b19e2551b168237872e3320a073ee004d3e
Dani <daniaxasabraxas@gmail.com>
2020-12-05 21:31:46 -0300

    Added loop to display data in ProductItem component

--- frontend/src/pages/products/ProductsList.vue
+++ frontend/src/pages/products/ProductsList.vue
@@ -1,8 +1,15 @@
 <template>
   <base-card card-mode="container mt-5 mb-5">
     <div class="row">
-      <product-item v-for="(prod) in fetchAllProducts" v-bind:key="prod.productId">
-        {{ prod }}
+      <product-item
+        v-for="(prod) in fetchAllProducts"
+        v-bind:key="prod.productId"
+        v-bind:product-id="prod.productId"
+        v-bind:product-title="prod.title"
+        v-bind:product-price="prod.price"
+        v-bind:product-description="prod.description"
+        v-bind:product-image="prod.imageUrl"
+        >
       </product-item>
     </div>
   </base-card>
