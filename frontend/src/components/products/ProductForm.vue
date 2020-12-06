135d34a0664b9b3c13a3d96c8915b8a5d23e1d2c
Dani <daniaxasabraxas@gmail.com>
2020-12-05 22:39:27 -0300

    Working on Form Styles

--- frontend/src/components/products/ProductForm.vue
+++ frontend/src/components/products/ProductForm.vue
@@ -1,40 +1,42 @@
 <template>
-  <form class="mx-auto" style="max-width: 300px;">
-    <div class="form-group">
-      <label for="productId">Product Id</label>
-      <input type="number" name="productId" id="productId" class="d-flex w-100"/>
-    </div>
-    <div class="form-group">
-      <label for="title">Title</label>
-      <input type="text" name="title" id="title" class="d-block"/>
-    </div>
-    <div class="form-group">
-      <label for="title">Price</label>
-      <input type="text" class="form-control"/>
-    </div>
-    <div class="form-group">
-      <label for="title">Description</label>
-      <input type="text" class="form-control"/>
-    </div>
-    <div class="form-group">
-      <label for="title">Url de la imagen</label>
-      <input type="text" class="form-control"/>
-    </div>
-    <!--
-    <div class="form-group">
-      <label for="title">Category</label>
-      <select v-model="selectedCategory">
-        <option v-for="category in fetchCategories" v-bind:key="category.categoryId">{{category.title}}</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="title">Band</label>
-    </div>
-    <div class="form-group">
-      <a class="btn btn-outline-success">SAVE</a>
-    </div>
-    -->
-  </form>
+  <base-card card-mode="container mt-5 mb-5">
+    <form class="mx-auto" style="max-width: 300px;">
+      <div class="form-group">
+        <label for="productId">Product Id</label>
+        <input type="number" name="productId" id="productId" class="d-flex w-100"/>
+      </div>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" class="d-block w-100"/>
+      </div>
+      <div class="form-group">
+        <label for="price">Price</label>
+        <input type="number" name="price" id="price" class="d-flex w-100"/>
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <textarea type="text" name="description" id="description" rows="5" cols="35"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="image">Image</label>
+        <input type="text" name="image" id="image" class="d-flex w-100"/>
+      </div>
+      <!--
+      <div class="form-group">
+        <label for="title">Category</label>
+        <select v-model="selectedCategory">
+          <option v-for="category in fetchCategories" v-bind:key="category.categoryId">{{category.title}}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="title">Band</label>
+      </div>
+      <div class="form-group">
+        <a class="btn btn-outline-success">SAVE</a>
+      </div>
+      -->
+    </form>
+  </base-card>
 </template>
 
 <script>
