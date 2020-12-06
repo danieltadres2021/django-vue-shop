c532bf5e03d7c3548fa020c2afa842dc02f37619
Dani <daniaxasabraxas@gmail.com>
2020-12-05 21:28:26 -0300

    Added carts to display data from the backend

--- frontend/src/components/products/ProductItem.vue
+++ frontend/src/components/products/ProductItem.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mt-4">
     <div class="card">
+      <img class="card-img-top" v-bind:src="productImage" alt="Card image" style="width:100%">
       <div class="card-body">
-        <h4 class="card-title">Jane Doe</h4>
-        <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+        <h4 class="card-title">{{ productTitle }}</h4>
+        <p class="card-text">{{ productDescription }}</p>
         <a href="#" class="btn btn-primary">See Profile</a>
       </div>
-      <img class="card-img-bottom" src="img_avatar6.png" alt="Card image" style="width:100%">
     </div>
   </div>
 </template>
@@ -27,6 +27,10 @@ export default {
       required: true
     },
     productDescription: {
+      type: String,
+      required: true
+    },
+    productImage: {
       type: String,
       required: true
     }
