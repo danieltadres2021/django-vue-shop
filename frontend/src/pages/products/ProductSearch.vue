599dd8b1ab7b86d5971a0368498bf35a8bbd6b23
Dani <daniaxasabraxas@gmail.com>
2020-12-07 23:05:47 -0300

    Using the find method to fetch the Vinyl by the name

--- frontend/src/pages/products/ProductSearch.vue
+++ frontend/src/pages/products/ProductSearch.vue
@@ -2,15 +2,51 @@
   <base-card card-mode="container mt-5 mb-5">
     <h4 class="text-center">Looking for a Vinyl?</h4>
     <div class="form-group d-flex justify-content-center">
-      <input type="text" class="form-control mt-3 custom-input" placeholder="Enter a Vinyl name"/>
+      <input
+        type="text"
+        class="form-control mt-3 custom-input"
+        placeholder="Enter a Vinyl name"
+        name="title"
+        id="title"
+        v-model.trim="productTitle"/>
+    </div>
+    <div class="form-group d-flex justify-content-center">
+      <button
+        class="btn btn-primary btn-sm"
+        v-on:click="searchByName">
+        Search
+      </button>
+    </div>
+
+    <div v-if="showData">
+      <h1>Hi devs!</h1>
     </div>
   </base-card>
 </template>
 
-<style scoped>
-
-.custom-input {
-  width: 500px;
+<script>
+export default {
+  data() {
+    return {
+      showData: false,
+      productTitle: null,
+      selectedProduct: null
+    }
+  },
+  methods: {
+    searchByName() {
+      //console.log(this.productTitle);
+      //console.log(this.$store.getters.getProducts);
+      this.selectedProduct = this.$store.getters.getProducts.find(
+        product => product.title === this.productTitle
+      );
+    }
+  }
 }
+</script>
 
+<style scoped>
+  .custom-input {
+    width: 500px;
+  }
 </style>
