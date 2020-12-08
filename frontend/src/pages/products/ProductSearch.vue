93bf24682bcb88f27fdb51a1525e59a058180ee6
Dani <daniaxasabraxas@gmail.com>
2020-12-07 21:47:46 -0300

    Working on ProductSearch router page

---
+++ frontend/src/pages/products/ProductSearch.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-card card-mode="container mt-5 mb-5">
+    <h4 class="text-center">Looking for a Vinyl?</h4>
+    <div class="form-group d-flex justify-content-center">
+      <input type="text" class="form-control mt-3 custom-input" placeholder="Enter a Vinyl name"/>
+    </div>
+  </base-card>
+</template>
+
+<style scoped>
+
+.custom-input {
+  width: 500px;
+}
+
+</style>
