aa9c02eb519a8136d0cfb800670dc66cac6ad6ae
Dani <daniaxasabraxas@gmail.com>
2020-12-08 20:59:09 -0300

    Added Delete method

--- frontend/src/pages/products/ProductDelete.vue
+++ frontend/src/pages/products/ProductDelete.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Product Delete Page!</h1>
+  <base-card card-mode="container mt-5 mb-5">
+    <h4 class="text-center">Delete a Vinyl?</h4>
+    <ul class="list-group mx-auto mt-5"
+        style="max-width: 500px;">
+        <li class="list-group-item d-flex justify-content-between"
+            v-for="(prod) in fetchAllProducts"
+            v-bind:key="prod.productId">
+
+            <div>
+              {{ prod.productId }}
+              {{ prod.title }}
+            </div>
+
+            <div>
+              <span v-on:click="deleteById(prod.productId)" class="badge badge-danger p-2 pointer-cursor">
+                <i class="fas fa-trash-alt"></i>
+                DELETE
+              </span>
+            </div>
+        </li>
+    </ul>
+  </base-card>
 </template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch('loadProducts');
+  },
+  computed: {
+    fetchAllProducts() {
+      return this.$store.getters.getProducts
+    }
+  },
+  methods: {
+    deleteById(value) {
+      this.$store.dispatch('deleteProductById', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.pointer-cursor {
+  cursor: pointer
+}
+</style>
