2eec011da117568b9ac355b6e83c75df1c7d488d
Dani <daniaxasabraxas@gmail.com>
2020-12-07 21:47:08 -0300

    Added SearchProduct Page into vue router config

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ProductsList from './pages/products/ProductsList.vue';
 import ProductRegistration from './pages/products/ProductRegistration.vue';
+import ProductSearch from './pages/products/ProductSearch.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -9,6 +10,7 @@ const router = createRouter({
     { path: '/', redirect: '/products' },
     { path: '/products', component: ProductsList },
     { path: '/register', component: ProductRegistration },
+    { path: '/search', component: ProductSearch },
     { path: '/:notFound(.*)', component: NotFound },
   ]
 })
